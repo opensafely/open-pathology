5f7b896515b2925010884d23906343a0948656d3
Alice Wong <alice.wong@thedatalab.org>
2024-12-10 16:54:59 +0000

    Use urljoin and delete utils.py

--- app/config.py
+++ app/config.py
@@ -7,11 +7,11 @@ MEASURES = {
                     Only tests results returned to GPs are included,
                     which will usually exclude tests requested while a person is in hospital and other settings like a private clinic.""",
         "classification": "recovery",
-        "codelist_endpoint": "opensafely/alanine-aminotransferase-alt-tests/2298df3e/",
+        "codelist_url": "opensafely/alanine-aminotransferase-alt-tests/2298df3e/",
         "explanation": """An ALT blood test is one of a group of liver function tests (LFTs) which are used to detect problems with the function of the liver.
                         It is often used to monitor patients on medications which may affect the liver or which rely on the liver to break them down within the body.
                         They are also tested for patients with known or suspected liver dysfunction.""",
-        "url_endpoints": {
+        "csv_urls": {
             "counts": "01GGZ127420DXX35BM0MMQNW8N/",
             "deciles": "01GGZ12739P6B7Z00QAJBTBKK3/",
             "top_5_code": "01GGWFEGKSB1ANPP4X5V2FM3FR/",
@@ -19,7 +19,7 @@ MEASURES = {
     }
 }
 
-URLS = {
+BASE_URLS = {
     "codelists": "https://www.opencodelists.org/codelist/",
     "published_data": "https://jobs.opensafely.org/service-restoration-observatory/sro-key-measures-dashboard/published/",
 }
