7362eb3f65c0376120c712b612a29e8371847793
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2025-02-17 15:08:31 +0000

    Adding tests for reference range and comparator boolean combination counts

--- sdr/test_dataset.py
+++ sdr/test_dataset.py
@@ -3,13 +3,14 @@ from dataset import dataset
 
 test_data = {
     1: { 
-        "clinical_events": [{'numeric_value': 7, "snomedct_code": "1013211000000103", "date": date(2020, 4, 1)}, {"snomedct_code": "1013211000000103", "date": date(2020, 4, 1)}],
-        "patients": {},
+        "clinical_events_ranges": [{'numeric_value': 30, "snomedct_code": "1013211000000103", "date": date(2020, 4, 1), "lower_bound": 23, "upper_bound": 76, "comparator": '<'}, 
+                                  {'numeric_value': 30, "snomedct_code": "1013211000000103", "date": date(2020, 4, 1), "lower_bound": 23, "upper_bound": 76, "comparator": '~'}],
+        "patients": {"sex": 'male'},
         "practice_registrations": {},
         "expected_in_population": True,
         "expected_columns": {
-            "codelist_event_count": 2,
-            "test_value_count": 1
+            "valT_equalF_diffT_uppT_lowT": 1,
+            "valT_equalT_diffF_uppT_lowT": 1,
         },
     }
 }
