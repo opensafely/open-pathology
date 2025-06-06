a63f6eb0ec85c7615cb16f336014d435f3e853f1
Jaidip Gill <78973716+JaidipGill@users.noreply.github.com>
2025-06-06 15:05:03 +0000

    Adding psa and vit measures

---
+++ analysis/config.py
@@ -0,0 +1,11 @@
+codelists = {'alt': 'codelists/opensafely-alanine-aminotransferase-alt-tests.csv',
+             'chol': 'codelists/opensafely-cholesterol-tests.csv',
+             'hba1c': 'codelists/opensafely-glycated-haemoglobin-hba1c-tests.csv', 
+             'rbc': 'codelists/opensafely-red-blood-cell-rbc-tests.csv', 
+             'sodium': 'codelists/opensafely-sodium-tests-numerical-value.csv',
+             'systol': 'codelists/opensafely-systolic-blood-pressure-qof.csv',
+             'vit_d': 'codelists/ardens-vitamin-d-level.csv',
+             'vit_d_ref': 'codelists/ardens-vitamin-d-level.csv',
+             'psa': 'codelists/opensafely-psa-numeric-value.csv',
+             'psa_ref': 'codelists/opensafely-psa-numeric-value.csv'}
+
--- analysis/measure_definition.py
+++ analysis/measure_definition.py
@@ -4,11 +4,11 @@ from datetime import date
 from ehrql import INTERVAL, Measures, codelist_from_csv, months
 from ehrql.tables.core import clinical_events as events
 from ehrql.tables.core import patients
-from ehrql.tables.tpp import practice_registrations as registrations
-
+from ehrql.tables.tpp import practice_registrations as registrations, clinical_events_ranges as ranges
+from config import codelists
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--codelist")
+parser.add_argument("--test")
 args = parser.parse_args()
 
 start_date = date(2018, 4, 1)
@@ -40,35 +40,62 @@ is_registered = registration.exists_for_patient()
 
 is_sex_recorded = patients.sex.is_in(["male", "female"])
 
+is_male = patients.sex.is_in(['male'])
 
 # Codelists
 # --------------------------------------------------------------------------------------
-codelist = codelist_from_csv(args.codelist, column="code")
+codelist_path = codelists[args.test]
+codelist = codelist_from_csv(codelist_path, column="code")
 codelist_events = events.where(
     events.snomedct_code.is_in(codelist) & events.date.is_during(INTERVAL)
 )
 has_codelist_event = codelist_events.exists_for_patient()
 last_codelist_event = codelist_events.sort_by(codelist_events.date).last_for_patient()
 
+# Conditions
+# --------------------------------------------------------------------------------------
+
+# Define reference range for vit_d and psa tests
+if args.test in ['vit_d_ref', 'psa_ref']:
+
+    if args.test == 'vit_d_ref':
+        is_outside_ref = ranges.numeric_value < ranges.lower_bound
+    elif args.test == 'psa_ref':
+        is_outside_ref = ranges.numeric_value > ranges.upper_bound
+
+    tests_outside_ref = ranges.where(
+        ranges.snomedct_code.is_in(codelist) & ranges.date.is_during(INTERVAL) & is_outside_ref
+    ).exists_for_patient()
+    
 
 # Measures
 # --------------------------------------------------------------------------------------
 measures = Measures()
-measures.configure_dummy_data(population_size=15000, legacy=True)
+measures.configure_dummy_data(population_size=100, legacy=True)
 measures.configure_disclosure_control(enabled=True)
-measures.define_defaults(
-    denominator=is_alive & is_adult & is_registered & is_sex_recorded
-)
 intervals = months(num_months(start_date, date.today())).starting_on(start_date)
+
+numerator = has_codelist_event
+denominator = is_alive & is_adult & is_registered & is_sex_recorded
+
+if args.test in ['psa', 'psa_ref']:
+    denominator = denominator & is_male
+
+if args.test in ['vit_d_ref', 'psa_ref']:
+    numerator = tests_outside_ref
+    denominator = numerator & denominator
+
+measures.define_defaults(
+    numerator = numerator,
+    denominator = denominator,
+    intervals = intervals
+)
+
 measures.define_measure(
     name="by_practice",
-    numerator=has_codelist_event,
-    intervals=intervals,
     group_by={"practice": registration.practice_pseudo_id},
 )
 measures.define_measure(
     name="by_snomedct_code",
-    numerator=has_codelist_event,
-    intervals=intervals,
     group_by={"snomedct_code": last_codelist_event.snomedct_code},
 )
